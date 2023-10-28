b24af1f85b4435eb413bb69ceb8b6ba9d839210f
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-10-29 01:41:18 +0300

    docs: create seperate file for spinner styles properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import "./styles.css"
-import Weather from "./components/Weather"
+import Weather from "./Components/Weather"
 
 function App() {
   const [showComponent, setShowComponent] = useState<boolean>(false)
